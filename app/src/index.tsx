65d5da000a8f09c568ce5e3957537e4d77d8d0c3
Daniel Cepeda <dcep93@gmail.com>
2025-07-09 17:52:08 -0400

    x

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import PleaseDateMe from "./pleasedateme";
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <PleaseDateMe />
   </React.StrictMode>
 );
 
---
+++ app/src/pleasedateme/index.tsx
@@ -0,0 +1,3 @@
+export default function PleaseDateMe() {
+  return <div>pleasedateme</div>;
+}
